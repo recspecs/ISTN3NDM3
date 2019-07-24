c61c61d1e13247baffc13e81316a61daa2fbc05b
ScThTony <tonymfb13@gmail.com>
2019-07-25 01:15:31 +0200

    Add project files.

---
+++ SVGSystemCSharp/GeneralManager.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace SVGSystemCSharp
+{
+    partial class GeneralManager
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TopPanelGM = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.BlueStripGM = new System.Windows.Forms.Panel();
+            this.LogoPanelGM = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button6 = new System.Windows.Forms.Button();
+            this.LeftPanelGM = new System.Windows.Forms.Panel();
+            this.LeftPanelBtnsGM = new System.Windows.Forms.Panel();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.TopPanelGM.SuspendLayout();
+            this.LogoPanelGM.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.LeftPanelGM.SuspendLayout();
+            this.LeftPanelBtnsGM.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TopPanelGM
+            // 
+            this.TopPanelGM.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.TopPanelGM.Controls.Add(this.panel2);
+            this.TopPanelGM.Controls.Add(this.BlueStripGM);
+            this.TopPanelGM.Controls.Add(this.LogoPanelGM);
+            this.TopPanelGM.Dock = System.Windows.Forms.DockStyle.Top;
+            this.TopPanelGM.Location = new System.Drawing.Point(0, 0);
+            this.TopPanelGM.Margin = new System.Windows.Forms.Padding(0);
+            this.TopPanelGM.Name = "TopPanelGM";
+            this.TopPanelGM.Size = new System.Drawing.Size(1538, 203);
+            this.TopPanelGM.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.DarkGray;
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 153);
+            this.panel2.Margin = new System.Windows.Forms.Padding(0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1536, 48);
+            this.panel2.TabIndex = 2;
+            // 
+            // BlueStripGM
+            // 
+            this.BlueStripGM.BackColor = System.Drawing.Color.DarkBlue;
+            this.BlueStripGM.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.BlueStripGM.Dock = System.Windows.Forms.DockStyle.Top;
+            this.BlueStripGM.Location = new System.Drawing.Point(0, 140);
+            this.BlueStripGM.Margin = new System.Windows.Forms.Padding(0);
+            this.BlueStripGM.Name = "BlueStripGM";
+            this.BlueStripGM.Size = new System.Drawing.Size(1536, 13);
+            this.BlueStripGM.TabIndex = 1;
+            // 
+            // LogoPanelGM
+            // 
+            this.LogoPanelGM.BackColor = System.Drawing.Color.DarkGray;
+            this.LogoPanelGM.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.LogoPanelGM.Controls.Add(this.panel1);
+            this.LogoPanelGM.Dock = System.Windows.Forms.DockStyle.Top;
+            this.LogoPanelGM.Location = new System.Drawing.Point(0, 0);
+            this.LogoPanelGM.Margin = new System.Windows.Forms.Padding(0);
+            this.LogoPanelGM.Name = "LogoPanelGM";
+            this.LogoPanelGM.Size = new System.Drawing.Size(1536, 140);
+            this.LogoPanelGM.TabIndex = 0;
+            this.LogoPanelGM.Paint += new System.Windows.Forms.PaintEventHandler(this.LogoPanelGM_Paint);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.button6);
+            this.panel1.Location = new System.Drawing.Point(1050, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(394, 58);
+            this.panel1.TabIndex = 0;
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Transparent;
+            this.button6.Dock = System.Windows.Forms.DockStyle.Left;
+            this.button6.FlatAppearance.BorderSize = 0;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.ForeColor = System.Drawing.Color.White;
+            this.button6.Location = new System.Drawing.Point(0, 0);
+            this.button6.Margin = new System.Windows.Forms.Padding(0);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(101, 54);
+            this.button6.TabIndex = 0;
+            this.button6.Text = "Settings";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.Button6_Click);
+            // 
+            // LeftPanelGM
+            // 
+            this.LeftPanelGM.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.LeftPanelGM.BackColor = System.Drawing.Color.DarkGray;
+            this.LeftPanelGM.Controls.Add(this.LeftPanelBtnsGM);
+            this.LeftPanelGM.Location = new System.Drawing.Point(0, 203);
+            this.LeftPanelGM.Margin = new System.Windows.Forms.Padding(0);
+            this.LeftPanelGM.Name = "LeftPanelGM";
+            this.LeftPanelGM.Padding = new System.Windows.Forms.Padding(20, 10, 20, 20);
+            this.LeftPanelGM.Size = new System.Drawing.Size(166, 727);
+            this.LeftPanelGM.TabIndex = 3;
+            // 
+            // LeftPanelBtnsGM
+            // 
+            this.LeftPanelBtnsGM.Controls.Add(this.button5);
+            this.LeftPanelBtnsGM.Controls.Add(this.button4);
+            this.LeftPanelBtnsGM.Controls.Add(this.button3);
+            this.LeftPanelBtnsGM.Controls.Add(this.button2);
+            this.LeftPanelBtnsGM.Controls.Add(this.button1);
+            this.LeftPanelBtnsGM.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.LeftPanelBtnsGM.Location = new System.Drawing.Point(20, 10);
+            this.LeftPanelBtnsGM.Margin = new System.Windows.Forms.Padding(0);
+            this.LeftPanelBtnsGM.Name = "LeftPanelBtnsGM";
+            this.LeftPanelBtnsGM.Size = new System.Drawing.Size(126, 697);
+            this.LeftPanelBtnsGM.TabIndex = 0;
+            // 
+            // button5
+            // 
+            this.button5.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button5.Location = new System.Drawing.Point(0, 392);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(126, 98);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button4.Location = new System.Drawing.Point(0, 294);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(126, 98);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button3.Location = new System.Drawing.Point(0, 196);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(126, 98);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button2.Location = new System.Drawing.Point(0, 98);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(126, 98);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 98);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // GeneralManager
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1538, 925);
+            this.Controls.Add(this.LeftPanelGM);
+            this.Controls.Add(this.TopPanelGM);
+            this.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "GeneralManager";
+            this.Text = "General Manager";
+            this.Load += new System.EventHandler(this.GeneralManager_Load);
+            this.Layout += new System.Windows.Forms.LayoutEventHandler(this.GeneralManager_Layout);
+            this.Resize += new System.EventHandler(this.GeneralManager_Resize);
+            this.TopPanelGM.ResumeLayout(false);
+            this.LogoPanelGM.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.LeftPanelGM.ResumeLayout(false);
+            this.LeftPanelBtnsGM.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel TopPanelGM;
+        private System.Windows.Forms.Panel LogoPanelGM;
+        private System.Windows.Forms.Panel BlueStripGM;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel LeftPanelGM;
+        private System.Windows.Forms.Panel LeftPanelBtnsGM;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ SVGSystemCSharp/GeneralManager.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace SVGSystemCSharp
+{
+    public partial class GeneralManager : Form
+    {
+        clsResize gmResize;
+
+        public GeneralManager()
+        {
+            InitializeComponent();
+            gmResize = new clsResize(this);
+            this.Load += new EventHandler(_Load);
+            this.Resize += new EventHandler(_Resize);
+
+        }
+
+        private static void ConfigureCrystalReports()
+        {
+        }
+
+        private void GeneralManager_Load(object sender, EventArgs e)
+        {
+            ConfigureCrystalReports();
+        }
+
+        private void _Load(object sender, EventArgs e)
+        {
+            gmResize._get_initial_size();
+        }
+
+        private void _Resize(object sender, EventArgs e)
+        {
+            gmResize._resize();
+        }
+
+
+
+        private void LogoPanelGM_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics graphics = e.Graphics;
+            Rectangle gradient_rectangle = new Rectangle(0, 0, LogoPanelGM.Width, LogoPanelGM.Height);
+            Brush b = new LinearGradientBrush(gradient_rectangle, Color.Gray, Color.DarkGray,LinearGradientMode.Vertical);
+            graphics.FillRectangle(b, gradient_rectangle);
+
+        }
+
+        private void GeneralManager_Layout(object sender, LayoutEventArgs e)
+        {
+            //this.SuspendLayout();
+            //LogoPanelGM.Height = this.Height / 10;
+           // BlueStripGM.Location = new Point(LogoPanelGM.Left, LogoPanelGM.Bottom);
+            //BlueStripGM.Size = new Size(LogoPanelGM.Width, LogoPanelGM.Height / 10);
+           // this.ResumeLayout();
+
+        }
+
+        private void Button6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GeneralManager_Resize(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
